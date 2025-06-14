4717607ff14ceabf60cc1043966c9c4d4ce45060
Souvik Sarkar <sarkorsouvo@gmail.com>
2025-06-14 18:57:07 +0530

    code updated

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from 'cors'
+import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
-import path from 'path'
+import path from "path";
 import { connectDb } from "./db/connectDB.js";
 const app = express();
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(cookieParser())
+app.use(cookieParser());
 dotenv.config();
-const Port = process.env.PORT || 3000
-const __dirname = path.resolve()
-if(process.env.NODE_ENV === "production"){
+const Port = process.env.PORT || 3000;
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  
+
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
 app.use("/api/auth", authRoutes);
